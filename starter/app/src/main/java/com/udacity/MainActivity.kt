309e06e47c3deb6f173374d387ba90b3b9472feb
castroDAMC <castro.damc@gmail.com>
2022-04-19 21:34:28 -0400

    content_main.xml - Main Layout MainActivity.kt - Send initial Notification strings.xml - Basic Strings

--- starter/app/src/main/java/com/udacity/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.udacity
 
-import android.app.DownloadManager
-import android.app.NotificationManager
-import android.app.PendingIntent
+import android.app.*
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.Color
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,9 +23,11 @@ class MainActivity : AppCompatActivity() {
     private var downloadID: Long = 0
 
     private lateinit var notificationManager: NotificationManager
+    private lateinit var builder: NotificationCompat.Builder
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,9 +35,39 @@ class MainActivity : AppCompatActivity() {
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
+        configureNotification()
+
         custom_button.setOnClickListener {
             download()
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun configureNotification() {
+        // NotificationManager
+        notificationManager = this.getSystemService(NotificationManager::class.java)
+
+        // NotificationChannel
+        val notificationChannel = NotificationChannel(
+            CHANNEL_ID,
+            "Resource Download Notification",
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        notificationChannel.enableLights(true)
+        notificationChannel.lightColor = Color.RED
+        notificationChannel.enableVibration(true)
+        notificationChannel.description = "Time for breakfast"
+
+        builder = NotificationCompat.Builder(
+            applicationContext,
+            CHANNEL_ID
+        ).setContentTitle(applicationContext.getString(R.string.notification_title)).setContentText(
+            getString(
+                R.string.notification_description
+            )
+        ).setSmallIcon(R.drawable.ic_assistant_black_24dp)
+
+        notificationManager.createNotificationChannel(notificationChannel)
     }
 
     private val receiver = object : BroadcastReceiver() {
@@ -42,6 +77,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun download() {
+
+
+        try {
+            notificationManager.notify(1, builder.build())
+        } catch (e: Exception){
+            Log.d("TEST", e.toString())
+        }
+
+
         val request =
             DownloadManager.Request(Uri.parse(URL))
                 .setTitle(getString(R.string.app_name))
